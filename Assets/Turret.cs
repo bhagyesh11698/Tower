2b77570762508aa93f6811bd5ccedc01463dd0d9
Bhagyesh Patel <11698guru@gmail.com>
2020-09-04 23:32:53 +0530

    Turrets
    - rotate particular part
    - draw gizmos in scene mode
    - gizmos of turret are set to range of turret
    - convert quaternion to euler
    - smoothly rotate to new target using lerp

---
+++ Assets/Turret.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Transform target;
+    public float range = 15f;
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
+
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distancetoEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distancetoEnemy < shortestDistance)
+            {
+                shortestDistance = distancetoEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy !=null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        //target lock on 
+        Vector3 dir = target.position - transform.position;
+        
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+
+        //convert quaternion to euler - x,y,z as shown in inspector
+        //lerp will smoothly rotate head from partToRotate to lookRotation with speed of 10
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation,lookRotation,Time.deltaTime * turnSpeed).eulerAngles;
+        
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red; 
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
