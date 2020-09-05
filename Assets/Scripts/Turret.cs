f4547fcfb0118e68150a313e25d6d0500215c26f
Bhagyesh Patel <11698guru@gmail.com>
2020-09-05 13:42:58 +0530

    Shooting
    - create bullet using sphere
    - create a point in turrent to instantiate bullets
    - particles effects not implemented

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform target;
+
+    [Header("Attributes")]
     public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountDown = 0f;
+
+    [Header("Unity Setup Fields")]
     public string enemyTag = "Enemy";
 
     public Transform partToRotate;
     public float turnSpeed = 10f;
 
+    public GameObject bulletPrefab;
+    public Transform firePoint;
     void Start()
     {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
@@ -59,6 +67,24 @@ public class Turret : MonoBehaviour
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation,lookRotation,Time.deltaTime * turnSpeed).eulerAngles;
         
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if (fireCountDown >= 0f)
+        {
+            Shoot();
+            fireCountDown = 1f / fireRate;
+        }
+        fireCountDown -= Time.deltaTime;
+
+    }
+
+    void Shoot()
+    {
+       GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+        if (bullet!=null)
+        {
+            bullet.Seek(target);
+        }
     }
 
     private void OnDrawGizmosSelected()
