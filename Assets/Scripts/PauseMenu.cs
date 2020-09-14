223310c8b1a2b57b75891731408b40aabf34743d
Bhagyesh Patel <11698guru@gmail.com>
2020-09-14 14:38:54 +0530

    Pause Menu
    - Created pause menu with retry, continue and menu button
    - Removed camera controller functionality
    - retry will restart level
    - continue will resume game
    
    - pause menu animation not included
    - if time is frozen, Keep animation working by changing update mode in animator to UnscaledTime

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject ui;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        //if enabled, this will disable and if disabled, it will enable
+        ui.SetActive(!ui.activeSelf);
+
+        if (ui.activeSelf)
+        {
+            // game will be paused including time.deltatime
+            // to double the speed of time in game, set time.fixeddeltatime to 2f  
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        //Time.timeScale = 1f;
+        // or 
+        Toggle();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+
+    }
+
+}
